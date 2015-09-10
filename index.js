08882028ba781946bfa332c69e667d67793090e2
parentin <jean-joffrey.parentin@inist.fr>
2015-09-10 13:40:51 +0200

    Rename module, Code grammar

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 /* global module */
 /*jslint node: true */
 /*jslint indent: 2 */
-(function () {
+;(function () {
   "use strict";
   module.exports = function dothejob(jsonLine, cb) {
     jsonLine.canvasOK = true;
-
     if (jsonLine.id1 === '2b6372af-c83c-4379-944c-f1bff3ab25d8') {
       jsonLine.canvasOK = false;
-      cb({
+      return cb({
         code: 1,
         message: 'J\'aime po cet ID là...'
       });
     } else {
-      cb();
+      return cb();
     }
   };
-
 }());
 
