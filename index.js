06f8848541e31e8b234722a417a2707903f4275b
Etienne Caron <etienne.caron@inist.fr>
2015-10-13 17:32:20 +0200

    Changement vers une variable business exportée (comme pour les autres modules)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* global module */
 /*jslint node: true */
 /*jslint indent: 2 */
-;(function () {
-  "use strict";
-  module.exports = function doTheJob(jsonLine, cb) {
+"use strict";
+
+var business = {};
+
+business.doTheJob = function (jsonLine, cb) {
     jsonLine.canvasOK = true;
     if (jsonLine.id1 === '2b6372af-c83c-4379-944c-f1bff3ab25d8') {
       jsonLine.canvasOK = false;
@@ -14,5 +16,6 @@
     } else {
       return cb();
     }
-  };
-}());
+};
+
+module.exports = business;
